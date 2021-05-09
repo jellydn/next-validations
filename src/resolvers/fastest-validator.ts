c6d89e34e44a0723f0d01cd3a1b495263b1fee6f
Huynh Duc Dung <dung@aircarbon.co>
2021-05-09 23:42:03 +0800

    :bug: Cannot find module 'fastest-validator'

--- src/resolvers/fastest-validator.ts
+++ src/resolvers/fastest-validator.ts
@@ -1,11 +1,14 @@
 import Validator, { ValidationError } from 'fastest-validator';
 
-export function fastestValidatorResolver<T>(schema: T) {
+function createValidator<T>(schema: T) {
   const validator = new Validator();
-  const check = validator.compile(schema);
+  return validator.compile(schema);
+}
 
+export function fastestValidatorResolver<T>(schema: T) {
   return {
     validate: (data: unknown): true | ValidationError[] => {
+      const check = createValidator(schema);
       const result = check(data);
       if (Array.isArray(result)) {
         throw result;

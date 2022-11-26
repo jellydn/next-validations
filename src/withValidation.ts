d198a4ccda9b6fd8569f31c84c86f7d703f14e0a
Huynh Duc Dung <dunghd.it@gmail.com>
2022-11-26 14:17:18 +0800

    refactor: DRY code for HOF

--- src/withValidation.ts
+++ src/withValidation.ts
@@ -5,7 +5,7 @@ import type { SCHEMA_TYPE } from "./validation";
 
 type NextHandler = (err?: Error) => void;
 
- type ValidationHoF = {
+type ValidationHoF = {
   type: SCHEMA_TYPE;
   mode?: "body" | "query" | "headers";
   schema: unknown;
@@ -16,31 +16,7 @@ export function withValidation({
   schema,
   mode = "query",
 }: ValidationHoF) {
-  return (
-    handler?: (req: NextApiRequest, res: NextApiResponse<any>) => any,
-  ) => {
-    return async (
-      req: NextApiRequest,
-      res: NextApiResponse,
-      next?: NextHandler,
-    ) => {
-      try {
-        const resolver = createResolver(type, schema);
-
-        resolver.validate(req[mode]);
-
-        if (!!next) {
-          return next();
-        }
-
-        if (handler) return handler(req, res);
-
-        res.status(404).end();
-      } catch (error) {
-        res.status(400).send(error);
-      }
-    };
-  };
+  return withValidations([{ type, schema, mode }]);
 }
 
 export function withValidations(validations: ValidationHoF[]) {

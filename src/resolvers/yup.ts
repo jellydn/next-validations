c26e7095ac499b1761663a77082100b44fd550cd
Huynh Duc Dung <dunghd.it@gmail.com>
2023-06-28 23:30:27 +0800

    fix: remove lazy type for yup

--- src/resolvers/yup.ts
+++ src/resolvers/yup.ts
@@ -1,9 +1,7 @@
-
 import * as Yup from 'yup';
-import type Lazy from 'yup/lib/Lazy';
 
-export function yupResolver<T extends Yup.AnyObjectSchema | Lazy<any>>(schema:T) {
-    return {
-        validate: (data: unknown) => schema.validateSync(data),
-    }
+export function yupResolver<T extends Yup.AnyObjectSchema>(schema: T) {
+  return {
+    validate: (data: unknown) => schema.validateSync(data),
+  };
 }
